8e2a7d0775773b6d9656a5eeb6c38e02df7a197e
arunkr21 <arunkr0038@gmail.com>
2021-06-20 14:36:46 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+    // preloader
+    $(window).on('load', function () {
+        $('.preloader').delay(1500).fadeOut(500);
+    });
+
+    // $(window).load(function() {
+    //     $('.preloader').fadeOut('slow');
+    // });
+// })
